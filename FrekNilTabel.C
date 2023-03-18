c1b8c6b4f13a439f4e6d6364e4b595bcd8a5d968
Naufal Rizki Saputra <117664936+naufalrs04@users.noreply.github.com>
2023-03-18 10:29:33 +0700

    Add files via upload

---
+++ FrekNilTabel.C
@@ -0,0 +1,38 @@
+/* Nama File    		: FrekNilTabel.C */
+/* Deskripsi    		: Menampilkan nilai elemen tabel T yang frekuensi kemunculannya lebih dari satu kali */
+/* Pembuat      		: Naufal Rizki Saputra - 24060122120011 */
+/* Tanggal Pembuatan	: 15 Maret 2023 */	
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main () {
+
+    /* Kamus */
+    int N,i,j, frekuensi ;
+
+    /* Algoritma */
+    printf ("Nilai N : ");
+    scanf ("%d", &N) ;
+
+    int T[N] ;
+    for (i = 1 ; i<=N ; i++) {
+        printf ("Elemen ke - %d : ",i) ;
+        scanf ("%d", &T[i]) ;
+    }
+
+    for (i = 1; i<=N ; i++) {
+        frekuensi = 1 ;
+        for (j = i + 1; j<=N ; j++) {
+            if (T[i] == T[j] && T[i] != 0) {
+                frekuensi = frekuensi + 1;
+                T[j] = 0 ;
+            }
+        }
+        if (frekuensi > 1) {
+            printf ("%d " , T[i]) ;
+        }
+    }
+
+    return 0 ;
+}
